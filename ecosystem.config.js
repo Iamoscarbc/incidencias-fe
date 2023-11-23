fd3893300564e1a334ed0be03cd1e9323e487fe5
unknown <oscarmbravoc@gmail.com>
2023-11-23 12:24:12 -0500

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-    apps: [
-      {
-        name: 'incidences-fe',
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/vite/bin/vite.js',
-        args: 'start'
-      }
-    ]
-  }
+  apps: [
+    {
+      name: `incidences-fe`,
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: "serve",
+      env: {
+        PM2_SERVE_PATH: "./dist",
+        PM2_SERVE_PORT: 80,
+        PM2_SERVE_SPA: "true",
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
